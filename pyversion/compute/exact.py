78e6447d8bf37734aeac73c649b51a0172786314
Rob Righter <rrighter@rrighte-ltm74fx.internal.salesforce.com>
2022-10-15 12:35:47 -0400

    started on e func

--- pyversion/compute/exact.py
+++ pyversion/compute/exact.py
@@ -1,4 +1,6 @@
 class Compute:
+    
+    
     def check_for_repeat(self, ret):
         sp = ret.split(".")
         if len(sp) == 0:
@@ -20,9 +22,12 @@ class Compute:
         ret=""
         first=True
         while (remainder > 0):
+            #print(remainder)
+            #print(ret)
             result = int(remainder/d)
             ret = ret+str(result)
             remainder = remainder - (result*d)
+            #print(remainder)
             if first:
                 if remainder != 0:
                     ret=ret+"."
@@ -33,3 +38,25 @@ class Compute:
                     return { 'repeat': True, 'value': ret, 'repeat_value': repeat }
             remainder=remainder*10
         return { 'repeat': False, 'value': ret }
+
+    def factorial(self, n):
+        ret = int(1);
+        while n > 0:
+            ret = ret * int(n)
+            n = n - 1
+        return ret
+
+    def e(self):
+        numerator = int(2)
+        denominator = int(1)
+        for i in range(2, 5):
+            fact = self.factorial(i)
+            next_denominator = fact * denominator
+            next_numerator = (numerator * fact) + (denominator * 1)
+            numerator = next_numerator
+            denominator = next_denominator
+            print(i)
+        print(str(numerator) + " / " + str(denominator) )
+        return self.divide(numerator, denominator)
+            
+        
--- pyversion/tests.py
+++ pyversion/tests.py
@@ -5,21 +5,33 @@ c = Compute()
 
 class TestComputeMethods(unittest.TestCase):
 
-    def test_divide(self):
-        self.assertEqual(c.divide(1,2323), { 'repeat': True, 'value': '0.0004304778303917348256564786913473956091261300043047783039173482565647869134739560912613', 'repeat_value': '00043047783039173482565647869134739560912613' })
-        self.assertEqual(c.divide(1,4), { 'repeat': False, 'value': '0.25'})
-        self.assertEqual(c.divide(1,3), { 'repeat': True, 'value': '0.33', 'repeat_value': '3'})
-        self.assertEqual(c.divide(9,3), { 'repeat': False, 'value': '3'})
-        self.assertEqual(c.divide(10,3), { 'repeat': True, 'value': '3.33', 'repeat_value': '3'})
+	def test_divide(self):
+		self.assertEqual(c.divide(1,2323), { 'repeat': True, 'value': '0.0004304778303917348256564786913473956091261300043047783039173482565647869134739560912613', 'repeat_value': '00043047783039173482565647869134739560912613' })
+		self.assertEqual(c.divide(1,4), { 'repeat': False, 'value': '0.25'})
+		self.assertEqual(c.divide(1,3), { 'repeat': True, 'value': '0.33', 'repeat_value': '3'})
+		self.assertEqual(c.divide(9,3), { 'repeat': False, 'value': '3'})
+		self.assertEqual(c.divide(10,3), { 'repeat': True, 'value': '3.33', 'repeat_value': '3'})
+		#self.assertEqual(c.divide(780,288), { 'repeat': True, 'value': '3.33', 'repeat_value': '3'})
 
+	def test_factorial(self):
+		self.assertEqual(c.factorial(0), 1)
+		self.assertEqual(c.factorial(1), 1)
+		self.assertEqual(c.factorial(2), 2)
+		self.assertEqual(c.factorial(3), 6)
+		self.assertEqual(c.factorial(4), 24)
+		self.assertEqual(c.factorial(5), 120)
+		self.assertEqual(c.factorial(9), 362880)
+
+	def test_factorial(self):
+		pass
+		#self.assertEqual(c.e(), "2.7182818284590452353602874713526624977572470936999595749669676277")
+
+#2 1 = 5 1
+#1 2   2 6
 if __name__ == '__main__':
     unittest.main()
-
-
-
-#value = c.divide(1,2323)
-#print("Value is: "+ value['value'])
-#if(value['repeat']):
-#	print("Decimal repeats with value: "+value['repeat_value'])
-	 
-#print('\n')
+	#value = c.divide(780,288)
+	#print("Value is: "+ value['value'])
+	#if(value['repeat']):
+	#	print("Decimal repeats with value: "+value['repeat_value'])
+	#print('\n')
